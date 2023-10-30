9971bd45e7ead45fbf4d5e96711c0a2e60039f43
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-10-30 15:10:08 +0530

    Solved the Appbar color not changing problem by using the Appbar theme.

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+
+  // static const primaryColor = Color(0xFF0A0D22);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primaryColor: const Color(0xFF0A0D22), // Why this not changing the color of the app bar
+          // primaryColor: primaryColor,
+        // Why this not changing the color of the app bar
+        appBarTheme: const AppBarTheme(color:Color(0xFF0A0D22)),  // Solved the Appbar color not changing problem
         scaffoldBackgroundColor: const Color(0xFF0A0D22),
         colorScheme: ColorScheme.fromSwatch().copyWith(  //accent color is derp
       secondary: Colors.purple),
-        textTheme: TextTheme(bodyLarge:  ),
+
       ),
       home: InputPage(),
     );
@@ -27,8 +31,7 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0A0D22), // I am confused here
+      appBar: AppBar(// I am confused here
         title: const Center
           (child: Text('BMI CALCULATOR')),
       ),

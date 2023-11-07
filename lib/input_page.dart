9df93a5cdb7f09c2f95492750ec114def2c89fe0
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-08 00:24:24 +0530

    Created two separate dart files for Reusablecard widget and the upperui widget.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'Reusable_card.dart';
 
 const Color activeCardcolour = Color(0xFF1D1E33);
 const Color bottomBarcolor = Color(0xFFEB1555);
@@ -68,50 +70,9 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class UpperUi extends StatelessWidget {
-  UpperUi({this.cardicon,this.cardtext});
-  final IconData? cardicon;
-  final String? cardtext;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          cardicon,
-          size: 100.0,
-          color: Colors.white,
-        ),
-        const SizedBox(
-          height: 15.0,
-        ),
-        Text(cardtext!,style: const TextStyle(fontSize: 18,color: Colors.grey),)
-      ],
-    );
-  }
-}
 
 // With help of flutter outline we have avoided writing code again n again.
 // Followin is a widget which returns the widget we want to use several times in our app
 //
 
-class Resuablecard extends StatelessWidget {
-    Resuablecard({required this.colour,this.cardChild});
-  final Color colour;
-  final Widget? cardChild;
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      height: 200,
-      width: 170,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),
-          color: colour,
-    ),
-      child: cardChild);
-  }
-}

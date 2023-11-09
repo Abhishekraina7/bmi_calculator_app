4b70d7cc825205f67699550a9cf20cc240790034
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-09 23:33:18 +0530

    Added a new file named constants.dart to store the const used in our project

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kactiveCardcolour = Color(0xFF1D1E33);
+const Color kinactiveCardcolour = Color(0xFF111328);
+const Color kbottomBarcolor = Color(0xFFEB1555);
+const kbottomBarheight = 80.0;

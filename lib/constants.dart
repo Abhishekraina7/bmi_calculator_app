0460406e708f55bcf1a8ffa72a517bb050a286d2
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-11 14:36:09 +0530

    Created the Slider part of the application and learned a new widget.

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ const Color kactiveCardcolour = Color(0xFF1D1E33);
 const Color kinactiveCardcolour = Color(0xFF111328);
 const Color kbottomBarcolor = Color(0xFFEB1555);
 const kbottomBarheight = 80.0;
+const TextStyle knumberstyle = TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold);
+const koverlaycolor = Color(0xFFEB1555);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,7 +15,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  Genders ? selectedGender;   //It is a null aware operator, it helps the expression to be evaluated as null without causing any error
+  Genders ? selectedGender;//It is a null aware operator, it helps the expression to be evaluated as null without causing any error
+  int height = 180;
 
   // Color maleCardColor = inactiveCardcolour;
   // Color femaleCardColor = inactiveCardcolour;
@@ -96,19 +97,46 @@ class _InputPageState extends State<InputPage> {
               width: 500,
               decoration: BoxDecoration(borderRadius: BorderRadius.circular(15.0),
                   color:  const Color(0xFF1D1E33)),
-              child: const Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("Height",style: TextStyle(color: Colors.white, fontSize: 20.0),),
-                  SizedBox(
+                  const Text("Height",style: TextStyle(color: Colors.white, fontSize: 20.0),),
+                  const SizedBox(
                     height: 10.0,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,  // In flutter if you want to align some text with some other text, you gotta specify how to do that and this helps in doing that.
                     children: <Widget>[
-                      Text("160",
-                        style:TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold) ,)
+                      Text(height.toString(),
+                        style:knumberstyle,
+                      ),
+                      const Text("cm",
+                      style: TextStyle(fontSize: 18.0 ),),
                     ],
+                  ),
+                  SliderTheme(
+                    data: const SliderThemeData(
+                        inactiveTrackColor: const Color(0xFF8D8E98),
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x1fEB1555),
+                        activeTrackColor: Color(0xFFFFFFFF)
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 0.0,
+                      max: 250.0,
+
+                      onChanged: (double newvalue){
+                        setState(() {
+                          height = newvalue.round();
+                        });
+
+
+                      },
+
+                    ),
                   ),
                 ],
 

be3b2b1aae03f343c4f0a33c3e46451c55c4cdb2
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-21 15:36:04 +0530

    Added a new file to display the results

---
+++ lib/results_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BMI Calculator'),
+      ),
+      body: Text('Helllo there'),
+    );
+  }
+}

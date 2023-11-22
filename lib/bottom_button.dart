0f0ffe204f603100dad418224c44ae58071acb67
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-22 14:28:55 +0530

    Added bottom_button.dart file for making a separate file of handling Bottombutton in clear and concise manner.

---
+++ lib/bottom_button.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap,required this.buttonName});
+  final Function onTap;
+  final String buttonName;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap as void Function(),
+      child: Container(
+        color: kbottomBarcolor,
+        margin: const EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: kbottomBarheight,
+        child:  Center(child: Text(buttonName, style: kBottomtextstyle),
+        ),
+      ),
+    );
+  }
+}

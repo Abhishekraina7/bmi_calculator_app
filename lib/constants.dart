f1c05fbc60f12fb65d28a28102bc8b034e205bd5
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-22 14:29:59 +0530

    Added the new constants in constants.dart file which were needed in results_page.dart while making its UI.

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,30 @@ const Color kactiveCardcolour = Color(0xFF1D1E33);
 const Color kinactiveCardcolour = Color(0xFF111328);
 const Color kbottomBarcolor = Color(0xFFEB1555);
 const kbottomBarheight = 80.0;
-const TextStyle knumberstyle = TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold);
 const koverlaycolor = Color(0xFFEB1555);
+
+const TextStyle knumberstyle = TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold);
+
+const kBottomtextstyle = TextStyle(
+    fontSize: 25.0,
+    fontWeight: FontWeight.bold);
+
+const kResulttitlestyle = TextStyle(
+    fontSize: 40.0,
+    fontWeight: FontWeight.bold);
+
+const kResultstatusstyle = TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.normal,
+  color: Color(0xFF24D876),
+);
+const kFinalstyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100.0,
+);
+const kSuggestionmessagestyle = TextStyle(
+  fontSize: 15.0,
+  fontWeight: FontWeight.normal,
+);

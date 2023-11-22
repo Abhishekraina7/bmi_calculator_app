23d76add7bd028a28b90532229bb0448f0078e9e
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-22 14:33:43 +0530

    Made the whole UI structure and added the BottomButton(which has the route back to the Main screen).

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,8 @@
+import 'package:bmi_calculator_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'Reusable_card.dart';
+import 'input_page.dart';
+import 'bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
@@ -6,18 +10,47 @@ class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('BMI Calculator'),
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: ()
-          {
-            Navigator.pop(context);
-          },
-          child: const Text('Jump Back'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+                child: const Text(' Your Result',style: kResulttitlestyle,),
+              ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Resuablecard(colour: kactiveCardcolour,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Normal',style: kResultstatusstyle,
+                ),
+                Text('18.9',
+                style: kFinalstyle,
+                ),
+                Text('Your BMI result is low, you should eat more calories',
+                  style: kSuggestionmessagestyle,
+                ),
+              ],
+            ),
+            ),
+
+          ),
+          BottomButton(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => InputPage()));
+              },
+              buttonName: 'RE-CALCULATE'),
+          ],
       ),
 
-    );
+      );
   }
 }

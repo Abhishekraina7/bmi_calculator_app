c3068333b4a794aa5a605d3bc9e6ad37855e4e84
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-23 19:04:12 +0530

    Created a new dart file named brain_of_app.dart which will have all the logic for calculating the BMI.

---
+++ lib/brain_of_app.dart
@@ -0,0 +1,50 @@
+
+
+import 'dart:math';
+
+class Calculations{
+
+  Calculations({this.height,this.weight});
+
+  final int ?height;
+  final int ?weight;
+  double _bmi = 0;
+
+  String calculatemBMI()
+  {
+   _bmi = weight! / pow(height ! /100, 2);
+    return _bmi.toStringAsFixed(1); //This function is able to convert the answer to a single decimal place
+  }
+
+ String getResult()
+ {
+   if(_bmi >= 25)
+     {
+       return 'Overweight';
+     }
+   else if(_bmi > 18.5 && _bmi < 25)
+     {
+       return 'Normal';
+     }
+   else{
+     return 'Underweight';
+   }
+
+ }
+  String getSuggestions()
+  {
+    if(_bmi >= 25)
+    {
+      return 'You are overwight, try to shed some weight for better health.';
+    }
+    else if(_bmi > 18.5 && _bmi < 25)
+    {
+      return 'You\'r weight is normal! Good job.';
+    }
+    else{
+      return 'You have lower than normal body weight. You can eat bit more';
+    }
+
+  }
+
+}
